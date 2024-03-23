ca284af08d6b3a832e76415b32042b846a63a4ed
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2024-03-23 09:04:58 -0300

    Last prototype update

---
+++ src/basic.ts
@@ -0,0 +1,1 @@
+import NodeRSA from "node-rsa";
--- src/rsa.ts
+++ src/rsa.ts
@@ -1,18 +1,67 @@
 import Express, { Send } from "express";
 import NodeRSA from "node-rsa";
 
-interface BasicRSASettings {
+export interface BasicRSASettings {
   keys?: {
     public: string;
     private: string;
-    format?:
-      | {
-          public: NodeRSA.Format;
-          private: NodeRSA.Format;
-        }
-      | NodeRSA.Format;
+    format?: BasicRSAKeyFormats | NodeRSA.Format;
   };
   bits: 512 | 1024 | 2048 | 4096;
+}
+
+type BasicRSAKeyFormats = {
+  public: NodeRSA.Format;
+  private: NodeRSA.Format;
+};
+
+/** Creates a standard instance of an RSA client with minimal functions to encrypt and decrypt data */
+export class BasicRSA {
+  encoder: NodeRSA;
+  decoder: NodeRSA;
+  keyFormats: BasicRSAKeyFormats;
+
+  constructor(setting: BasicRSASettings) {
+    this.keyFormats =
+      typeof setting.keys?.format === "string"
+        ? {
+            public: setting.keys.format as NodeRSA.Format,
+            private: setting.keys.format as NodeRSA.Format,
+          }
+        : (setting.keys?.format as BasicRSAKeyFormats);
+
+    this.encoder = setting.keys
+      ? new NodeRSA(setting.keys.public, "public" ?? this.keyFormats?.public)
+      : new NodeRSA({ b: setting.bits });
+    this.decoder = setting.keys
+      ? new NodeRSA(setting.keys.private, "private" ?? this.keyFormats?.private)
+      : this.encoder;
+  }
+
+  encrypt(data: string) {
+    return this.encoder.encrypt(data, "base64");
+  }
+
+  decrypt(data: string) {
+    return this.decoder.decrypt(data, "utf8");
+  }
+
+  encryptWithKey(key: string, data: string, format?: NodeRSA.Format) {
+    const customEncoder = new NodeRSA(key, format);
+    return customEncoder.encrypt(data, "base64");
+  }
+
+  exportKey() {
+    return this.encoder.exportKey("public");
+  }
+
+  private toUTF(data: string) {
+    return Buffer.from(data, "base64").toString("utf8");
+  }
+
+  private toBase64(data: string) {
+    return Buffer.from(data).toString("base64");
+  }
 }
 
 interface ServerRSASettings extends BasicRSASettings {
@@ -41,76 +90,46 @@ interface WrapedSend extends Send {
   ___preserved: Send;
 }
 
-/** Creates a standard instance of an RSA client with minimal functions to encrypt and decrypt data
- */
-export class BasicRSA {
-  #encoder: NodeRSA;
-  #decoder: NodeRSA;
-
-  constructor(setting: BasicRSASettings) {
-    const keyFormats =
-      typeof setting.keys?.format === "string"
-        ? {
-            public: setting.keys.format as string,
-            private: setting.keys.format as string,
-          }
-        : setting.keys?.format;
-
-    this.#encoder = setting.keys
-      ? new NodeRSA(setting.keys.public, "public" ?? keyFormats?.public)
-      : new NodeRSA({ b: setting.bits });
-    this.#decoder = setting.keys
-      ? new NodeRSA(setting.keys.private, "private" ?? keyFormats?.private)
-      : this.#encoder;
-  }
-
-  encrypt(data: string) {
-    return this.#encoder.encrypt(data, "base64");
-  }
-
-  decrypt(data: string) {
-    return this.#decoder.decrypt(data, "utf8");
-  }
-
-  encryptWithKey(key: string, data: string) {
-    const customEncoder = new NodeRSA(this.toUTF(key));
-    return customEncoder.encrypt(data, "base64");
-  }
-
-  exportKey() {
-    return this.#encoder.exportKey("public");
-  }
-
-  private toUTF(data: string) {
-    return Buffer.from(data, "base64").toString("utf8");
-  }
-
-  private toBase64(data: string) {
-    return Buffer.from(data).toString("base64");
-  }
-}
+type ClientRSAEvent = "connect" | "connecting";
+type ClientRSASubscribe = { cb: () => any; action: ClientRSAEvent };
 
 export class ClientRSA extends BasicRSA {
-  serverKey: string;
+  serverData: { key: string; format: NodeRSA.Format };
   #serverKeyLoadedPromise: Promise<Response>;
+  #subscribes: ClientRSASubscribe[];
 
   constructor(settings: ClientRSASettings) {
     super(settings);
 
-    this.serverKey = "";
+    this.serverData = { key: "", format: "public" };
     this.#serverKeyLoadedPromise = {} as Promise<Response>;
+    this.#subscribes = [];
+  }
+
+  on(action: "connect" | "connecting", cb: () => void) {
+    this.#subscribes.push({
+      action,
+      cb,
+    });
   }
 
   async fetch(input: string | URL | Request, options?: RequestInit) {
     await this.#serverKeyLoadedPromise;
-
     return new Promise((resolve, reject) => {
+      console.info(this.serverData);
+
       fetch(input, {
         ...options,
-        body: JSON.stringify({
-          ___RSADATA: options?.body,
-          ___CLIENTKEY: this.exportKey(),
-        } as ServerRSARequestBody),
+        body: options?.body
+          ? JSON.stringify({
+              ___RSADATA: this.encryptWithKey(
+                this.serverData.key,
+                JSON.stringify(options?.body),
+                this.serverData.format
+              ),
+              ___CLIENTKEY: this.exportKey(),
+            } as ServerRSARequestBody)
+          : undefined,
       })
         .then(async (response) => {
           try {
@@ -140,11 +159,18 @@ export class ClientRSA extends BasicRSA {
     this.#serverKeyLoadedPromise = new Promise((resolve) => {
       const RSA = this;
       (function tryIt() {
+        RSA.event("connecting");
+
         fetch(publicKeyEndpoint, options)
           .then(async (res) => {
             try {
-              RSA.serverKey = await res.text();
-              resolve(res);
+              const data: any = await res.json();
+
+              if (data.key && data.format) {
+                RSA.serverData = data;
+                RSA.event("connect");
+                resolve(res);
+              }
             } catch (err) {
               console.error("> can't connect to RSA server: ", err);
               setTimeout(tryIt, 1000);
@@ -156,6 +182,19 @@ export class ClientRSA extends BasicRSA {
           });
       })();
     });
+  }
+
+  private event(action: ClientRSAEvent) {
+    this.#subscribes = this.#subscribes
+      .map((subscribe) => {
+        if (subscribe.action === action) {
+          subscribe.cb();
+          return true;
+        }
+
+        return subscribe;
+      })
+      .filter((s) => s !== true) as ClientRSASubscribe[];
   }
 }
 
@@ -169,31 +208,27 @@ export class ServerRSA extends BasicRSA {
 
   propagateKey(): Express.RequestHandler {
     return (req, res) => {
-      res.contentType("text");
-      return res.status(200).send(this.exportKey());
+      if (typeof res.contentType === "function") res.contentType("text");
+      if (typeof res.status === "function") res.status(200);
+      return res.json({
+        key: this.exportKey(),
+        format: this.keyFormats.public,
+      });
     };
   }
 
   handler(): Express.RequestHandler {
-    return (req, res, next) => {
+    const RSA = this;
+
+    return async (req, res, next) => {
       Object.assign(req, { RSA: this });
 
-      req.body = "";
-      req.on("data", (chunk) => (req.body += chunk));
-
-      req.on("end", () => {
-        const refuseOrNext = (condition: boolean) => {
-          if (condition) {
-            return res.status(400).send("request refused by rsa service");
-          }
-          return next();
-        };
-
+      function resolveRequest() {
         if (typeof req.body === "string") {
           try {
             req.body = JSON.parse(req.body);
           } catch {
-            return refuseOrNext(this.allowUnencryptedIn);
+            return refuseOrNext(RSA.allowUnencryptedIn);
           }
         }
 
@@ -209,13 +244,14 @@ export class ServerRSA extends BasicRSA {
 
           return (data: any) => {
             try {
+    
               const encrypted = RSA.encryptWithKey(
                 ___CLIENTKEY,
                 typeof data === "string" ? data : JSON.stringify(data)
               );
 
               return originalSend.___preserved.call(
-                this,
+                RSA,
                 JSON.stringify({
                   ___RSADATA: encrypted,
                 })
@@ -223,28 +259,50 @@ export class ServerRSA extends BasicRSA {
             } catch (err) {
               res.status(500);
               return originalSend.___preserved.call(
-                this,
+                RSA,
                 "Error trying to encrypt response data"
               );
             }
           };
         };
 
-        (res.send as any) = wrapSend(res.send.bind(res) as WrapedSend, this);
+        (res.send as any) = wrapSend(res.send.bind(res) as WrapedSend, RSA);
 
         Object.assign(req, { CLIENTKEY: ___CLIENTKEY });
         if (___RSADATA) {
           try {
-            (req.body as any) = this.decrypt(___RSADATA as string);
+            (req.body as any) = RSA.decrypt(___RSADATA as string);
+            console.info("next called");
             return next();
           } catch {
             console.info("payload encrypted with a unknown key");
-            return refuseOrNext(this.allowUnencryptedIn);
+            return refuseOrNext(RSA.allowUnencryptedIn);
           }
         }
 
+        console.info("next called");
+
         next();
+      }
+
+      const refuseOrNext = (condition: boolean) => {
+        if (condition) {
+          return res.status(400).send("request refused by rsa service");
+        }
+        console.info("next called");
+        return next();
+      };
+
+      if (!req.readable) return resolveRequest();
+
+      req.body = "";
+
+      req.on("data", (chunk) => {
+        console.info("receiving data");
+        req.body += chunk;
       });
+
+      req.on("end", resolveRequest);
     };
   }
 }
